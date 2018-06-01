ed1eb3b69ad9813e274089c67b5720d8c6da02be
Daniel Rubio <drubio@cash-converters.es>
2018-06-01 09:58:37 +0200

    variables api strava y remodelado de directorios

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -31,7 +31,15 @@ $(function () {
 
 });
 
-
-// https://www.strava.com/oauth/authorize?client_id=26016&response_type=code&redirect_uri=http://localhost&scope=view_private
-// 1cd663b63f446e88ae16377f4ed0c3c75cbb4d0e
-// https://www.strava.com/api/v3/athlete/activities?before=1527775498&after=1527689164
+var idapp = '26016';
+var secret = '43476f02df28fd7a7a5f6cd7aba18618d3f41b5d';
+var redirect = 'http://localhost';
+var linkAuth = 'https://www.strava.com/oauth/authorize?client_id='+idapp+'&response_type=code&redirect_uri='+redirect;
+// el link obtiene el 'code' que hay que post para obtener id de atleta y access token
+var code = '84e062718e072ebcc13dd06e72021e82a87afc1a';
+var postAthlete = 'https://www.strava.com/oauth/token?client_id='+idapp+'&client_secret='+secret+'&code='+code;
+var access_token = '7db9e521c0b278d1aee0f76b38c700e128e76a28';
+var ahora = Math.round(new Date().getTime()/1000.0);
+var ayer = ahora - 86400;
+var getActividad = 'https://www.strava.com/api/v3/athlete/activities?before='+ahora+'&after='+ayer+'&access_token='+access_token;
+var getAthlete = 'https://www.strava.com/api/v3/athlete?access_token='+access_token;
