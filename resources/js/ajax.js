a8b7aa1c3c895494afe9cbbc1949fb8a15716ada
Daniel Rubio <drubio@cash-converters.es>
2018-06-01 10:05:36 +0200

    funciona

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -6,7 +6,7 @@ $(function () {
             r.forEach(function (e) {
                 elemento.push('<div class="col-md-4">' +
                     '<div class="card">' +
-                    '<img class="card-img-top" src="resources/' + e.img_proyecto + '" alt="' + e.proyecto + '">' +
+                    '<img class="card-img-top" src="resources/img/' + e.img_proyecto + '" alt="' + e.proyecto + '">' +
                     '<div class="card-body">' +
                     '<h5 class="card-title">' + e.proyecto + '</h5>' +
                     '<p>¡Faltan ' + (e.objetivo - e.contribucion) + 'km para la meta!</p>' +
@@ -35,7 +35,6 @@ var idapp = '26016';
 var secret = '43476f02df28fd7a7a5f6cd7aba18618d3f41b5d';
 var redirect = 'http://localhost';
 var linkAuth = 'https://www.strava.com/oauth/authorize?client_id='+idapp+'&response_type=code&redirect_uri='+redirect;
-// el link obtiene el 'code' que hay que post para obtener id de atleta y access token
 var code = '84e062718e072ebcc13dd06e72021e82a87afc1a';
 var postAthlete = 'https://www.strava.com/oauth/token?client_id='+idapp+'&client_secret='+secret+'&code='+code;
 var access_token = '7db9e521c0b278d1aee0f76b38c700e128e76a28';
